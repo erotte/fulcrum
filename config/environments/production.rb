f675601475be5f5b039049d94bb25e6362b4d457
Eckhard Rotte <erotte@gmail.com>
2011-09-02 15:42:42 +0200

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Fulcrum::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-  
+  # 
   # config.action_mailer.smtp_settings = {
   #  :address => "localhost" ,
   #  :port => 25,
